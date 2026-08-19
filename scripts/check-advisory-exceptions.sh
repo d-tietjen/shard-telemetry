2a3256e7bf83797cba1839ca42f6e918d46cc304
Devon Tietjen <devon@eden.dev>
2026-08-18 21:08:37 -0400

    ci: make advisory path check color independent

--- scripts/check-advisory-exceptions.sh
+++ scripts/check-advisory-exceptions.sh
@@ -6,7 +6,7 @@ cd "$repo_root"
 
 expected=$'bincode v1.3.3\nlrpar v0.13.10\nlrlex v0.13.10\npromql-parser v0.10.0\nshard-telemetry v0.1.0'
 observed="$({
-  cargo tree --locked -e normal -i bincode@1.3.3 --prefix none |
+  CARGO_TERM_COLOR=never cargo tree --locked -e normal -i bincode@1.3.3 --prefix none |
     sed -e '/^promql-parser v0.10.0 (\*)$/d' \
         -e "s#shard-telemetry v0.1.0 (.*)#shard-telemetry v0.1.0#"
 })"
